b45939481bbb483f830ce0ac110ab1cae07f0fc2
Bishworaj Poudel <thulotechnology@gmail.com>
2023-02-14 19:12:26 +0545

    Second Day

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:incometaxcalculatornepal/screens/splash_screen.dart';
 
+void main() {
+  runApp(SplashScreen());
+}
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -2,27 +2,28 @@
 // 20 for couple
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:incometaxcalculatornepal/controller/function.dart';
 
 void main() {
-  //group('Individual Testing', () {
-  //   test('Individual Testing 1',
-  //       () => expect(taxCalculateIndividual(1000), [10]));
-  //   test('Individual Testing 2',
-  //       () => expect(taxCalculateIndividual(500000), [5000]));
-  //   test('Individual Testing 2',
-  //       () => expect(taxCalculateIndividual(715000), [5000, 20000, 3000]));
-  //   test(
-  //       'Individual Testing 3',
-  //       () => expect(
-  //           taxCalculateIndividual(1225000), [5000, 20000, 60000, 67500]));
-  //   test(
-  //       'Individual Testing 4',
-  //       () => expect(
-  //           taxCalculateIndividual(1921000), [5000, 20000, 60000, 276000]));
+  group('Individual Testing', () {
+    test('Individual Testing 1',
+        () => expect(taxCalculateIndividual(1000), [10]));
+    test('Individual Testing 2',
+        () => expect(taxCalculateIndividual(500000), [5000]));
+    test('Individual Testing 2',
+        () => expect(taxCalculateIndividual(715000), [5000, 20000, 3000]));
+    test(
+        'Individual Testing 3',
+        () => expect(
+            taxCalculateIndividual(1225000), [5000, 20000, 60000, 67500]));
+    test(
+        'Individual Testing 4',
+        () => expect(
+            taxCalculateIndividual(1921000), [5000, 20000, 60000, 276000]));
 
-  //   test(
-  //       'Individual Testing 5',
-  //       () => expect(taxCalculateIndividual(2500000),
-  //           [5000, 20000, 60000, 300000, 180000]));
-  // });
+    test(
+        'Individual Testing 5',
+        () => expect(taxCalculateIndividual(2500000),
+            [5000, 20000, 60000, 300000, 180000]));
+  });
 }
