5c505f36f9d132429a6c6135dfe91413313b3b96
manishghorashainee <manishghorashainee18@gmail.com>
2023-02-14 18:03:12 +0545

    working

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:incometaxcalculatornepal/screens/home_screen.dart';
+import 'package:incometaxcalculatornepal/screens/test.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:incometaxcalculatornepal/utils/config.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  AppDetails appDetails = AppDetails();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Text(AppDetails.appName),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+
+          //Data Table for Couple
+          child: DataTable(border: TableBorder.all(), columns: const [
+            DataColumn(
+                label:
+                    Text("Slab", style: TextStyle(fontWeight: FontWeight.bold)),
+                numeric: true),
+            DataColumn(
+                label: Text("Cumulative",
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            DataColumn(
+                label: Text("Rate",
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            DataColumn(
+                label: Text("Taxable Amount",
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            DataColumn(
+                label: Text("Tax @ Slab Rate",
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+          ], rows: const [
+            DataRow(cells: [
+              DataCell(Text("600,000")),
+              DataCell(Text("600,000")),
+              DataCell(Text("1%")),
+              DataCell(Text("600,000")),
+              DataCell(Text("")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text("200,000")),
+              DataCell(Text("800,000")),
+              DataCell(Text("10%")),
+              DataCell(Text("200,000")),
+              DataCell(Text("")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text("300,000")),
+              DataCell(Text("11, 00,000")),
+              DataCell(Text("20%")),
+              DataCell(Text("300,000")),
+              DataCell(Text("")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text("1,100,000")),
+              DataCell(Text("2,000,000")),
+              DataCell(Text("30%")),
+              DataCell(Text("1,100,000")),
+              DataCell(Text("")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text(">20,00,000")),
+              DataCell(Text("")),
+              DataCell(Text("36%")),
+              DataCell(Text("500,000")),
+              DataCell(Text("")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text("")),
+              DataCell(Text("")),
+              DataCell(Text("")),
+              DataCell(Text("Grand Total",
+                  style: TextStyle(fontWeight: FontWeight.bold))),
+              DataCell(
+                Text(""),
+              ),
+            ]),
+          ]),
+        ),
+      ],
+    );
   }
 }
