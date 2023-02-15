da7ea15a61530258795eba21f7bf06fba3a6d9b7
Bishworaj Poudel <thulotechnology@gmail.com>
2023-02-15 18:50:26 +0545

    Upto Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,58 @@
-import 'package:flutter/widgets.dart';
-import 'package:incometaxcalculatornepal/screens/splash_screen.dart';
+import 'dart:async';
 
-void main() {
-  runApp(SplashScreen());
+import 'package:flutter/material.dart';
+import 'package:incometaxcalculatornepal/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/logo.png",
+            scale: 5,
+          )
+        ],
+      ),
+    )
+
+        /* Container(
+        color: Colors.blue,
+        child: const Center(
+            child: Center(
+          child: Text(
+            "Tax Calculator Nepal",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+        )),
+      ),*/
+        );
+  }
 }
